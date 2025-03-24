3b9b20cf48588224a05d7ae252718ccaf15f3c88
AngryShrimp <johnstone.keenan@gmail.com>
2025-03-24 17:46:01 -0600

    fix: get-versions

--- .github/workflows/get-versions.js
+++ .github/workflows/get-versions.js
@@ -1,22 +1,3 @@
-const fs = require('fs');
+let fs = require('fs');
 
-// Path to your JSON file
-const filePath = './module.json'; // Change this to your actual JSON file
-
-// Read and parse the JSON file
-fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error('Error reading the file:', err);
-        return;
-    }
-    try {
-        const jsonData = JSON.parse(data);
-        if (jsonData.version) {
-            console.log('Version:', jsonData.version);
-        } else {
-            console.error('Version key not found in JSON file.');
-        }
-    } catch (parseErr) {
-        console.error('Error parsing JSON:', parseErr);
-    }
-});
+console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
