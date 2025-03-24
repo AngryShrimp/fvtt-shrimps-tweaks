f31ae560455f688afdffeae65f7dea5794de8d6b
AngryShrimp <johnstone.keenan@gmail.com>
2025-03-24 17:58:41 -0600

    fix: auto-release

--- .github/workflows/get-versions.js
+++ .github/workflows/get-versions.js
@@ -1,3 +1,37 @@
 let fs = require('fs');
 
-console.log(JSON.parse(fs.readFileSync('./module.json', 'utf8')).version)
+let filename = './module.json';
+let version = JSON.parse(fs.readFileSync(filename, 'utf8')).version;
+// Read the JSON file
+fs.readFile(filename, 'utf8', (err, data) => {
+    if (err) {
+        console.error('Error reading the file:', err);
+        return;
+    }
+    
+    try {
+        // Parse JSON
+        let jsonData = JSON.parse(data);
+
+        let version = jsonData.version;
+        jsonData.manifest = `https://github.com/AngryShrimp/fvtt-shrimps-tweaks/releases/download/${version}/module.json`;
+        jsonData.download = `https://github.com/AngryShrimp/fvtt-shrimps-tweaks/releases/download/${version}/module.zip`;
+
+        // Convert back to JSON string with formatting
+        const updatedJson = JSON.stringify(jsonData, null, 2);
+
+        // Write back to the file
+        fs.writeFile(filename, updatedJson, 'utf8', (writeErr) => {
+            if (writeErr) {
+                console.error('Error writing to file:', writeErr);
+                return;
+            }
+            console.log('JSON file successfully updated!');
+        });
+
+    } catch (parseErr) {
+        console.error('Error parsing JSON:', parseErr);
+    }
+});
+
+console.log(version);
